9be531ef5f518aceb53fe002fae10fbcd30caf3a
Danny Wolf <wolfd@users.noreply.github.com>
2017-12-04 17:39:22 -0800

    Create google-chrome.sh

---
+++ installs/google-chrome.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd ~/Downloads
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+sudo apt install -fy
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+rm google-chrome-stable_current_amd64.deb
